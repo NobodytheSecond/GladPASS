cdc187807176dc3e4ecfb82a441bcbbe6f43564a
NobodytheSecond <128249894+NobodytheSecond@users.noreply.github.com>
2024-04-03 19:29:59 -0400

    Update TBAInterface.js

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "JMqWEzUAwi6Jb2Y7A7okCeJgvpNVaxJ9d3ZEQ3jAE853vrR9O2BNddcY0XfToOmb";
 /**
  * Get list of teams in event
  *
