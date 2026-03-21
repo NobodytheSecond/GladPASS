89ff631a68d41e5c2f9e27e412f2d0cba9d159c8
NobodytheSecond <128249894+NobodytheSecond@users.noreply.github.com>
2026-03-20 21:58:54 -0400

    Update rebuilt_config.js

--- 2026/rebuilt_config.js
+++ 2026/rebuilt_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2026MDPAS",
+      "defaultValue": "2026MDBET",
       "required": "true"
     },
     { "name": "Match Level",
